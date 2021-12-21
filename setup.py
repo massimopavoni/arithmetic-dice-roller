b786ff665eaef6943f56cf446b898f2341017760
Massimo Pavoni <maspavoni@gmail.com>
2021-12-21 14:02:07 +0100

    (Version changed)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name='arithmetic-dice-roller',
-    version='0.2.0',
+    version='0.2.1',
     description="A handy dice roller with extended notation and arithmetic expressions management.",
     long_description=README,
     long_description_content_type="text/markdown",
