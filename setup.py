f03d34fe7fae9cb8359f476d2b30f242ec9dae9d
Massimo Pavoni <maspavoni@gmail.com>
2022-12-13 13:22:26 +0100

    0.2.4 - License copyright notice in source files + actual sympy update

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
+"""
+arithmetic-dice-roller
+Copyright (C) 2021  Massimo Pavoni
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from pathlib import Path
 
 from setuptools import setup, find_packages
@@ -6,7 +24,7 @@ README = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name='arithmetic-dice-roller',
-    version='0.2.3',
+    version='0.2.4',
     description="A handy dice roller with extended notation and arithmetic expressions management.",
     long_description=README,
     long_description_content_type="text/markdown",
